09b9d1a7cd7019d11583b7944c1cb3fe43de929b
José Aniceto <jose.pedro.aniceto@gmail.com>
2020-01-16 22:22:32 +0000

    Improved style

--- gui.py
+++ gui.py
@@ -1,47 +1,56 @@
 from tkinter import *
 from tkinter import filedialog
+import tkinter.font as font
 import os
 import file_organizer as fo
 
+
+main_font = ('Segoe UI Light', 12)
+code_font = ('Consolas', 12)
 
 class Application(Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         master.title('File Organizer')  # Width height
-        master.geometry("700x400")  # Set window size
+        master.geometry('455x550')  # Set window size
+        master.configure(background='white')
         self.create_widgets()  # Create widgets/grid
         self.working_dir = fo.Directory()
         self.target_dir = fo.Directory()
         
     def create_widgets(self):
+        # Title
+        self.title = Label(self.master, text='File Organizer', font=('Segoe UI Light', 20), bg='white', padx=10, pady=10)
+        self.title.grid(row=0, column=0, sticky=W)
+
         # Working Directory
-        self.working_lb = Label(self.master, text="1) Working directory:", padx=10, pady=10)
-        self.working_lb.grid(row=0, column=0, sticky=W)
+        self.working_lb = Label(self.master, text='1) Working directory:', font=main_font, bg='white', padx=10, pady=10)
+        self.working_lb.grid(row=1, column=0, sticky=W)
         self.working_text = StringVar()
-        self.working_entry = Entry(self.master, textvariable=self.working_text, width=50)
-        self.working_entry.grid(row=1, column=0, padx=10)
-        self.select_btn = Button(self.master, text="Select directory", command=self.select_working_dir)
-        self.select_btn.grid(row=1, column=1)
+        self.working_entry = Entry(self.master, textvariable=self.working_text, font=code_font, width=40, relief='flat', state='readonly')
+        self.working_entry.grid(row=2, column=0, padx=10, ipady=5)
+        self.select_btn = Button(self.master, text='Browse', font=main_font, relief='flat', command=self.select_working_dir)
+        self.select_btn.grid(row=2, column=1)
 
         # Subdirectory selection list (sudirectories to merge)
-        self.subdirs_list_lb = Label(self.master, text="2) Select directories to merge:", padx=10, pady=10)
-        self.subdirs_list_lb.grid(row=2, column=0, sticky=W)
-        self.subdirs_listbox = Listbox(self.master, selectmode='multiple', width=50)
-        self.subdirs_listbox.grid(row=3, column=0, sticky=W, padx=10)
+        self.subdirs_list_lb = Label(self.master, text='2) Select directories to merge:', font=main_font, bg='white', padx=10, pady=10)
+        self.subdirs_list_lb.grid(row=3, column=0, sticky=W)
+        self.subdirs_listbox = Listbox(self.master, selectmode='multiple', width=40, font=code_font, relief='flat')
+        self.subdirs_listbox.grid(row=4, column=0, sticky=W, padx=10)
 
         # Target directory
-        self.target_lb = Label(self.master, text="3) Target directory for content:", padx=10, pady=10)
-        self.target_lb.grid(row=4, column=0, sticky=W)
+        self.target_lb = Label(self.master, text='3) Target directory for content:', font=main_font, bg='white', padx=10, pady=10)
+        self.target_lb.grid(row=5, column=0, sticky=W)
         self.target_text = StringVar()
-        self.target_entry = Entry(self.master, textvariable=self.target_text, width=50)
-        self.target_entry.grid(row=5, column=0, sticky=N, padx=10)
-        self.select_btn = Button(self.master, text="Select directory", command=self.select_target_dir)
-        self.select_btn.grid(row=5, column=1, sticky=N)
+        self.target_entry = Entry(self.master, textvariable=self.target_text, width=40, font=code_font, relief='flat', state='readonly')
+        self.target_entry.grid(row=6, column=0, sticky=W, padx=10, ipady=5)
+        self.select_btn = Button(self.master, text='Browse', font=main_font, relief='flat', command=self.select_target_dir)
+        self.select_btn.grid(row=6, column=1, sticky=N)
 
         # Merge files button
-        self.merge_btn = Button(self.master, text="Merge content!", command=self.merge_content)
-        self.merge_btn.grid(row=6, column=0, sticky=W, padx=10)
+        self.merge_btn = Button(self.master, text='Merge content!', font=main_font, relief='flat', command=self.merge_content)
+        self.merge_btn.grid(row=7, column=0, sticky=W, padx=10, pady=20)
 
     def select_target_dir(self):
         # Get directory
